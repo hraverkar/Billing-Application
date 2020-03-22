c010aedb7fa1ec752cac11cbf8bc17e8009ce1ca
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-22 20:57:05 +0530

    Login component changed code added.

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit, Inject, Renderer2 } from "@angular/core";
+import { Component, OnInit, Inject, Renderer2, ViewEncapsulation } from "@angular/core";
 import { RequestConfigService } from "../shared/requestConfig.service";
 import { Router, ActivatedRoute } from "@angular/router";
 import { MatSnackBar, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AleartdialogComponent } from '../aleartdialog/aleartdialog.component';
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  styleUrls: ["./login.component.css"],
+  encapsulation: ViewEncapsulation.None,
+  providers: [MatDialog]
 })
 export class LoginComponent implements OnInit {
   public hide = true;
   public userName: string;
   public password: string;
+  message = {
+    'title' : "TextError Technologies",
+    'address' : "Bengaluru",
+    'desingBy': "Design By : Harshal Raverkar"
+    }
+    
   constructor(
     private requestConfigService: RequestConfigService,
     private router: Router,
     private renderer: Renderer2,
     private snackBar: MatSnackBar,
     private route: ActivatedRoute,
+    private dialog: MatDialog,
+    public dialogRef: MatDialogRef<AleartdialogComponent>,
     @Inject(MAT_SNACK_BAR_DATA) public dataSnackBar: any
   ) {}
 
@@ -49,5 +61,15 @@ export class LoginComponent implements OnInit {
   onCancelClick() {
     this.userName = null;
     this.password = null;
+  }
+
+  onClickfab(){
+    let dialogRef = this.dialog.open(AleartdialogComponent, {
+      width: '30%',
+      height: '30%',
+      data: this.message
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 }
