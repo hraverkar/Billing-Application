876926af442b5829008d8e49b5a8381a3a275159
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-22 20:55:48 +0530

    Home component changed code added.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public customerName:string;
+  public customerAdd:string;
+  public customerNum:string;
+  public itemName:string;
+  public itemQuantity:number;
+  public itemPrice:number;
+  public totalPrice:number;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCancelClick(){
+    this.customerName=null;
+    this.customerAdd = null;
+    this.customerNum = null;
+    this.itemName = null;
+    this.itemQuantity = null;
+    this.itemPrice = null;
+    this.totalPrice = null;
+  }
+
+  onPrintClick(){
+    console.log("Hello Harhsl Raverkar")
   }
 
 }
