1f68cc99fc135b5c2d48ddb53d98b4bc43ddfc0f
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-22 20:58:06 +0530

    Request config import changed.

--- client/src/app/shared/requestConfig.service.spec.ts
+++ client/src/app/shared/requestConfig.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RequestConfigService } from './request-config.service';
+import { RequestConfigService } from './requestConfig.service'
 
 describe('RequestConfigService', () => {
   let service: RequestConfigService;
