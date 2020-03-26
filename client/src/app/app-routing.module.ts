961cabed45fc08bf6bec82c604283eb74dc79bf8
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-26 21:41:03 +0530

    Invoice history router page added into project.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { HistoryComponent } from './history/history.component';
 
 
 const routes: Routes = [
@@ -16,7 +17,11 @@ const routes: Routes = [
   {
     path: '',
     component: LoginComponent
-  }
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
 ];
 
 @NgModule({
