0a943e8549c585365cd15177be3e6e48175ea3c6
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-26 21:47:35 +0530

    Logout button added with router.

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onHomeClick() {
+    this.router.navigate(["home"], {});
   }
 
+  onHistoryClick() {
+    this.router.navigate(["history"], {});
+  }
+
+  logout() {
+    this.router.navigate(["login"], {});
+  }
 }
